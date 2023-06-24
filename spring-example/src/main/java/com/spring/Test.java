c0536189d4afa915fbf27421ce70047ea2ae94db
NULL2048 <973071263@qq.com>
2023-06-24 23:09:15 +0800

    补充注释

--- spring-example/src/main/java/com/spring/Test.java
+++ spring-example/src/main/java/com/spring/Test.java
@@ -6,8 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context
 				= new AnnotationConfigApplicationContext();
+		// 将Bean.class作为配置类，注册到容器中
 		context.register(Bean.class);
 		context.refresh();
+		// 从容器中获取Bean
 		System.out.println(context.getBean(Bean.class));
 	}
 }
